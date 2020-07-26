78ec992e833a06c2f0563751ea1debd4d35ad1ae
Gabriel Nucci <gabrielcfn@gmail.com>
2020-07-25 21:36:34 -0300

    Realizar chamada http ao carregar FilmeDetalhe

--- src/FilmeDetalhe/FilmeDetalhe.js
+++ src/FilmeDetalhe/FilmeDetalhe.js
@@ -1,15 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 
 const FilmeDetalhe = (props) => {
-  const uriDetalhe = `http://omdbapi.com/?apikey=ca9a6fca?i=`;
+  const uriDetalhe = `http://omdbapi.com/?apikey=ca9a6fca&i=`;
 
-  console.log(props);
+  const imdbID = props.match.params.imdbID;
+  const [filmePesquisado, setFilmePesquisado] = useState("");
+
+  useEffect(() => {
+    buscarDetalhes();
+  });
+
+  const buscarDetalhes = () => {
+    fetch(`${uriDetalhe}${imdbID}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // sucesso
+        if (data.Response === "True") {
+          console.log(data);
+          setFilmePesquisado(data);
+        } else {
+          // falha
+          console.log("erro", data);
+          return data;
+        }
+      });
+  };
 
   return (
     <div>
       <p>FilmeDetalhe</p>
+      <p>{filmePesquisado.Actors}</p>
+
       <Button component={RouterLink} to={`/pesquisar`}>
         Botão
       </Button>
